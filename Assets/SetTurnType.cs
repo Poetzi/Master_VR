efe33557e2b6381018c4175420218ac8dd502f74
Stephan Pölzl <p.stephan@gmx.at>
2024-03-19 14:43:11 +0100

    Finished Template Version

---
+++ Assets/SetTurnType.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SetTurnType : MonoBehaviour
+{
+    public ActionBasedSnapTurnProvider snapTurnProvider;
+    public ActionBasedContinuousTurnProvider continuousTurnProvider;
+
+    public void SetTypeFromIndex(int index)
+    {
+        if(index == 0)
+        {
+            snapTurnProvider.enabled = false;
+            continuousTurnProvider.enabled = true;
+        }
+        else if(index == 1)
+        {
+            snapTurnProvider.enabled = true;
+            continuousTurnProvider.enabled = false;
+        }
+    }
+}
